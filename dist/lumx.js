db5cde343535cb8574e051a8afe88ddea25ffa83
nir shilon <shilon5@gmail.com>
2016-11-03 11:04:53 +0200

    notification

--- dist/lumx.js
+++ dist/lumx.js
@@ -1276,81 +1276,6 @@
     'use strict';
 
     angular
-        .module('lumx.fab')
-        .directive('lxFab', lxFab)
-        .directive('lxFabTrigger', lxFabTrigger)
-        .directive('lxFabActions', lxFabActions);
-
-    function lxFab()
-    {
-        return {
-            restrict: 'E',
-            templateUrl: 'fab.html',
-            scope: true,
-            link: link,
-            controller: LxFabController,
-            controllerAs: 'lxFab',
-            bindToController: true,
-            transclude: true,
-            replace: true
-        };
-
-        function link(scope, element, attrs, ctrl)
-        {
-            attrs.$observe('lxDirection', function(newDirection)
-            {
-                ctrl.setFabDirection(newDirection);
-            });
-        }
-    }
-
-    function LxFabController()
-    {
-        var lxFab = this;
-
-        lxFab.setFabDirection = setFabDirection;
-
-        ////////////
-
-        function setFabDirection(_direction)
-        {
-            lxFab.lxDirection = _direction;
-        }
-    }
-
-    function lxFabTrigger()
-    {
-        return {
-            restrict: 'E',
-            require: '^lxFab',
-            templateUrl: 'fab-trigger.html',
-            transclude: true,
-            replace: true
-        };
-    }
-
-    function lxFabActions()
-    {
-        return {
-            restrict: 'E',
-            require: '^lxFab',
-            templateUrl: 'fab-actions.html',
-            link: link,
-            transclude: true,
-            replace: true
-        };
-
-        function link(scope, element, attrs, ctrl)
-        {
-            scope.parentCtrl = ctrl;
-        }
-    }
-})();
-(function()
-{
-    'use strict';
-
-    angular
         .module('lumx.dropdown')
         .directive('lxDropdown', lxDropdown)
         .directive('lxDropdownToggle', lxDropdownToggle)
@@ -1802,6 +1727,81 @@
                 $timeout.cancel(timer);
                 element.off();
             });
+        }
+    }
+})();
+(function()
+{
+    'use strict';
+
+    angular
+        .module('lumx.fab')
+        .directive('lxFab', lxFab)
+        .directive('lxFabTrigger', lxFabTrigger)
+        .directive('lxFabActions', lxFabActions);
+
+    function lxFab()
+    {
+        return {
+            restrict: 'E',
+            templateUrl: 'fab.html',
+            scope: true,
+            link: link,
+            controller: LxFabController,
+            controllerAs: 'lxFab',
+            bindToController: true,
+            transclude: true,
+            replace: true
+        };
+
+        function link(scope, element, attrs, ctrl)
+        {
+            attrs.$observe('lxDirection', function(newDirection)
+            {
+                ctrl.setFabDirection(newDirection);
+            });
+        }
+    }
+
+    function LxFabController()
+    {
+        var lxFab = this;
+
+        lxFab.setFabDirection = setFabDirection;
+
+        ////////////
+
+        function setFabDirection(_direction)
+        {
+            lxFab.lxDirection = _direction;
+        }
+    }
+
+    function lxFabTrigger()
+    {
+        return {
+            restrict: 'E',
+            require: '^lxFab',
+            templateUrl: 'fab-trigger.html',
+            transclude: true,
+            replace: true
+        };
+    }
+
+    function lxFabActions()
+    {
+        return {
+            restrict: 'E',
+            require: '^lxFab',
+            templateUrl: 'fab-actions.html',
+            link: link,
+            transclude: true,
+            replace: true
+        };
+
+        function link(scope, element, attrs, ctrl)
+        {
+            scope.parentCtrl = ctrl;
         }
     }
 })();
